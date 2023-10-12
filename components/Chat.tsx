7768ef8ca82413f7012c3f81a8f238a007b55397
abhishek mishra <abhms200@gmail.com>
2023-10-12 22:34:01 +0530

    chat page design

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,37 +1,71 @@
 import React, { useState } from 'react';
 
-const Chat = (selectedUser:any) => {
-  const [messages, setMessages] = useState([
-    { text: 'Hello, how are you?', sent: false },
-    { text: 'I\'m good, thanks!', sent: true },
+interface Message {
+  text: string;
+  type: 'sent' | 'received';
+}
+
+const Chat = (selectedUser: any) => {
+  const [messages, setMessages] = useState<Message[]>([
+    { text: 'Hello, how are you?', type: 'received' },
+    { text: 'I\'m good, thanks9999999999999!', type: 'sent' },
     // Add more messages as needed
   ]);
-console.log(selectedUser.selectedUser,"selectedUser");
+  const [messageText, setMessageText] = useState('');
+  const [isSendButtonEnabled, setIsSendButtonEnabled] = useState(false);
+
+  const handleSendMessage = () => {
+    if (messageText) {
+      const newMessage: Message = { text: messageText, type: 'sent' };
+      setMessages([...messages, newMessage]);
+      // Logic to send the message to the recipient here
+
+      // Clear the input field and disable the send button
+      setMessageText('');
+      setIsSendButtonEnabled(false);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value;
+    setMessageText(text);
+    setIsSendButtonEnabled(!!text);
+  };
+
+  console.log(selectedUser.selectedUser, "selectedUser");
+
   return (
-    <div className='chatt' >    
-    <div className="chat-container">
-      <div className="chat-header">
-        <img src={selectedUser.selectedUser.img} alt="Receiver Avatar" className="receiver-avatar" />
-       
-        <h2>{selectedUser.selectedUser.name}</h2> 
-      </div>
-      <div className="chat-messages">
-        <div className="message-scroll">
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              className={`message ${message.sent ? 'sent-message' : 'received-message'}`}
-            >
-              {message.text}
-            </div>
-          ))}
+    <div className='chatt'>
+      <div className="chat-container">
+        <div className="chat-header">
+          <img src={selectedUser?.selectedUser?.img} alt="Receiver Avatar" className="receiver-avatar" />
+          <h2>{selectedUser?.selectedUser?.name}</h2>
         </div>
+        <div className="chat-messages">
+          <div className="message-scroll">
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={`message ${message.type === 'sent' ? 'sent-message allsent' : 'received-message'}`}
+              >
+                {message.text}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="message-input">
+  <input
+    type="text"
+    placeholder="Type a message..."
+    value={messageText}
+    className='inputmessage' // Apply the inputmessage style here
+    onChange={handleInputChange}
+  />
+  <button onClick={handleSendMessage} disabled={!isSendButtonEnabled} className="button-send">
+    Send
+  </button>
+</div>
       </div>
-      <div className="message-input">
-        {/* Add your input form here */}
-        <input/>
-      </div>
-    </div>
     </div>
   );
 };
