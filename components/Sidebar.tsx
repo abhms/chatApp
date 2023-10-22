ee3af46a65eca2655bfdf8c282486562a525e136
abhishek mishra <abhms200@gmail.com>
2023-10-22 19:47:07 +0530

    remove comment

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,120 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import Chat from '../components/Chat';
-// import { useSelector, useDispatch } from 'react-redux';
-// import allUser from '../utils/allUser';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import getApproval from '@/utils/getApproval';
-// import { setAooroved } from '../redux/slices/approved';
-
-// type User = {
-//   firstname: string;
-//   lastname: string;
-//   profilePic: string;
-//   // Add other properties as needed
-// };
-
-// const Sidebar = () => {
-//   const dispatch = useDispatch();
-//   const { users } = useSelector((state: any) => state.user);
-//   const [userData, setUserData] = useState<User[]>([]);
-//   const [selectedUser, setSelectedUser] = useState<User | null>(null);
-//   const [searchQuery, setSearchQuery] = useState<string>('');
-//   const [filteredUsers, setFilteredUsers] = useState<User[]>(userData);
-
-//   const handleSearch = () => {
-//     if (searchQuery === '') {
-//       setFilteredUsers(userData); // Reset to all users when search is empty
-//     } else {
-//       const filtered = userData.filter((user) => {
-//         const fullName = `${user.firstname} ${user.lastname}`;
-//         return fullName.toLowerCase().includes(searchQuery.toLowerCase());
-//       });
-
-//       setFilteredUsers(filtered);
-//     }
-//   };
-
-//   useEffect(() => {
-//     allUser()
-//       .then((data) => {
-//         setUserData(data);
-//         setFilteredUsers(data); 
-//       })
-//       .catch((error) => {
-//         console.error("An error occurred:", error);
-//       });
-//   }, []);
-
-//   useEffect(() => {
-//     const token = localStorage.getItem('token');
-
-//     const getApprovalData = async () => {
-//       const approvalData = await getApproval(token);
-//       console.log(approvalData, "approvalData");
-//       dispatch(setAooroved(approvalData));
-//     };
-
-//     getApprovalData();
-//   }, [dispatch]);
-
-//   const handleUserClick = (user: User) => {
-//     setSelectedUser(user);
-//   };
-
-//   return (
-//     <>
-//       <div className="sidebar">
-//         <input
-//           className="searchbox"
-//           placeholder="Search..."
-//           value={searchQuery}
-//           onChange={(e) => {
-//             setSearchQuery(e.target.value), handleSearch();
-//           }}
-//           onKeyUp={(e) => {
-//             if (e.key === 'Enter') {
-//               handleSearch();
-//             }
-//           }}
-//         />
-//       </div>
-//       <div className="sidebar">
-//         <div className="sidebar-users">
-//           {userData.length === 0 ? (
-//             <CircularProgress disableShrink />
-//           ) : (
-//             filteredUsers.map((user, index) => (
-//               <div className="user" key={index} onClick={() => handleUserClick(user)}>
-//                 <div className="user-image">
-//                   <img
-//                     className="img"
-//                     src={user.profilePic}
-//                     alt={`${user.firstname} ${user.lastname}`}
-//                   />
-//                 </div>
-//                 <div className="user-info">
-//                   <button className="user-button" onClick={() => handleUserClick(user)}>
-//                     <span>
-//                       {user.firstname} {user.lastname}
-//                     </span>
-//                   </button>
-//                 </div>
-//               </div>
-//             ))
-//           )}
-//         </div>
-//       </div>
-
-//       <div className="">
-//         {selectedUser && <Chat selectedUser={selectedUser} />}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Sidebar;
-
-
 import React, { useState, useEffect } from 'react';
 import Chat from '../components/Chat';
 import { useSelector, useDispatch } from 'react-redux';
@@ -127,7 +10,7 @@ type User = {
   firstname: string;
   lastname: string;
   profilePic: string;
-  // Add other properties as needed
+
 };
 
 const Sidebar = () => {
@@ -143,8 +26,8 @@ const Sidebar = () => {
     setSearchQuery(query);
 
     if (query === '') {
-      setFilteredUsers(userData); // Reset to all users when search is empty
-      setNotFound(false); // Clear "Not Found" message
+      setFilteredUsers(userData);
+      setNotFound(false);
     } else {
       const filtered = userData.filter((user) => {
         const fullName = `${user.firstname} ${user.lastname}`;
@@ -165,7 +48,7 @@ const Sidebar = () => {
     allUser()
       .then((data) => {
         setUserData(data);
-        setFilteredUsers(data); // Display all users by default
+        setFilteredUsers(data);
       })
       .catch((error) => {
         console.error("An error occurred:", error);
@@ -196,7 +79,7 @@ const Sidebar = () => {
           placeholder="Search..."
           value={searchQuery}
           onChange={(e) => {
-            handleSearch(e.target.value); // Trigger search onChange
+            handleSearch(e.target.value);
           }}
         />
       </div>
@@ -205,7 +88,7 @@ const Sidebar = () => {
           {userData.length === 0 ? (
             <CircularProgress disableShrink />
           ) : notFound ? (
-            <p style={{color:"black"}}>No users found.</p>
+            <p style={{ color: "black" }}>No users found.</p>
           ) : (
             filteredUsers.map((user, index) => (
               <div className="user" key={index} onClick={() => handleUserClick(user)}>
