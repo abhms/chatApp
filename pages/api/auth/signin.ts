f533d17144438cfd48a665d36df6c0d31228d2d8
abhishek mishra <abhms200@gmail.com>
2024-01-14 00:44:12 +0530

    testing

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -19,6 +19,7 @@ export default async function handler(
         return res.status(401).json({ error: 'Incorrect Email Address' });
       }
 
+      
       const passwordMatch = await bcrypt.compare(password, user.password);
 
       if (!passwordMatch) {
